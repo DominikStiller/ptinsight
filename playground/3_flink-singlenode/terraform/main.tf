ce04ff677db9941248183a024d737667f64f5bed
Dominik Stiller <dominik.stiller@hpe.com>
2020-05-11 15:08:01 +0200

    Refactor Terraform file and restrict security group

--- playground/3_flink-singlenode/terraform/main.tf
+++ playground/3_flink-singlenode/terraform/main.tf
@@ -1,40 +1,66 @@
+# --------------------------------------------
+#      Input Variables
+# --------------------------------------------
+
 variable "ssh_key" {
-    default = "~/.ssh/id_rsa_eda_deployer"
+    description = "SSH key for EC2 instances"
 }
+
+variable "trusted_cidr" {
+    description = "CIDRs which are allowed to access security groups"
+}
+
 
 provider "aws" {
     region = "eu-central-1"
 }
 
+
+
+# --------------------------------------------
+#      EC2 instances
+# --------------------------------------------
+
 resource "aws_instance" "instance" {
-    ami = "ami-0be110ffd53859e30"
-    instance_type = "t3.micro"
+    ami                    = "ami-0be110ffd53859e30"
+    instance_type          = "t3.micro"
     vpc_security_group_ids = [aws_security_group.security_group.id]
-    key_name = aws_key_pair.key.key_name
+    key_name               = aws_key_pair.key.key_name
+
+    root_block_device {
+        delete_on_termination = true
+    }
 
     tags = {
-        Name = "eda-flink"
+        Name = "eda_flink"
         project = "eda"
     }
 }
 
-resource "aws_security_group" "security_group" {
-    name = "eda-dev"
 
-    # SSH
+
+# --------------------------------------------
+#      Security
+# --------------------------------------------
+
+resource "aws_security_group" "security_group" {
+
+    name = "eda_dev"
+
     ingress {
+        description = "SSH from trusted CIDRs"
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.trusted_cidr
     }
 
-    # Flink Web UI
     ingress {
+        description = "Flink Web UI from trusted CIDRs"
         from_port   = 8081
         to_port     = 8081
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.trusted_cidr
     }
 
     egress {
@@ -50,7 +76,8 @@ resource "aws_security_group" "security_group" {
 }
 
 resource "aws_key_pair" "key" {
-    key_name = "eda-deployer"
+
+    key_name   = "eda_deploy"
     public_key = file("${var.ssh_key}.pub")
 
     tags = {
@@ -58,6 +85,11 @@ resource "aws_key_pair" "key" {
     }
 }
 
-output "host" {
+# --------------------------------------------
+#      Output Values
+# --------------------------------------------
+
+
+output "flink_host" {
     value = aws_instance.instance.public_dns
 }
---
+++ playground/3_flink-singlenode/terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+trusted_cidr = ["46.223.0.0/16"]
+ssh_key = "~/.ssh/id_rsa_eda_deployer"
