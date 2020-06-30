75b9c887470927e89cc7370105ceba0461b1d992
Dominik Stiller <dominik.stiller@hpe.com>
2020-06-30 14:00:49 +0200

    Ignore generated protobuf files

--- app/common/python/ptinsight/common/proto/__init__.py
+++
@@ -1,6 +0,0 @@
-# AUTO-GENERATED, DO NOT EDIT!
-from ptinsight.common.proto.base_pb2 import *
-from ptinsight.common.proto.egress.counts_pb2 import *
-from ptinsight.common.proto.egress.delays_pb2 import *
-from ptinsight.common.proto.egress.flow_pb2 import *
-from ptinsight.common.proto.ingress.hsl_realtime_pb2 import *
