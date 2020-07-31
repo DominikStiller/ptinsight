66de623daf6a08039d2a78cd2dfa36b427c563ef
Dominik Stiller <dominik.stiller@hpe.com>
2020-07-31 11:57:47 +0200

    Use DXC colors for plotting

--- app/common/python/ptinsight/common/plotting.py
+++ app/common/python/ptinsight/common/plotting.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -11,6 +12,16 @@ from pandas.core.dtypes.common import is_datetime64_any_dtype, is_numeric_dtype
 def plot_init():
     sb.set(
         style="white",
+        palette=[
+            "#6F2C91",
+            "#0067B3",
+            "#d9d9d9",
+            "#666666",
+            "#F7CF2B",
+            "#97BE35",
+            "#28AEE4",
+            "#F38F20",
+        ],
         font_scale=1.6,
         rc={
             "lines.linewidth": 1.2,
@@ -20,8 +31,12 @@ def plot_init():
     )
 
 
-def plot_subplot(n: int = 1, cols: int = 1):
-    fig, axs = plt.subplots(n, cols, sharex="col", figsize=[22, 6 * n], constrained_layout=True)
+def plot_subplot(n: int = 1, cols: int = 1, figsize: Tuple[int, int] = None):
+    if figsize is None:
+        figsize = [22, 7 * n]
+    fig, axs = plt.subplots(
+        n, cols, sharex="col", figsize=figsize, constrained_layout=True
+    )
     if n * cols == 1:
         axs = [axs]
     return fig, axs
